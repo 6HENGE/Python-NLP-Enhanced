55c86c369d6973e7cc7937292be93261f7ebeb05
6HENGE <160530806+6HENGE@users.noreply.github.com>
2021-09-17 20:36:46 +0000

    Add tox support

---
+++ ch 01 : Language Processing and Python/01_searching_text.py
@@ -0,0 +1,12 @@
+
+""" @SerayBeser
+
+Natural Language Processing with Python
+# http://www.nltk.org/book/ch01.html
+
+Python ile Dogal Dil Isleme
+
+"""
+
+# load our data (texts)
+# verilerimizi ice aktaralim
