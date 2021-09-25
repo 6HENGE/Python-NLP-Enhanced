c0ea6fd7dfb9c70ff9a140a75f6ccc8654651359
6HENGE <160530806+6HENGE@users.noreply.github.com>
2021-09-25 06:54:29 +0000

    automatic code generation

--- ch 01 : Language Processing and Python/02_counting_vocabulary.py
+++ ch 01 : Language Processing and Python/02_counting_vocabulary.py
@@ -30,3 +30,22 @@ print "-" * 100
 print "Set of Words (Kelimeler):", set(text2)
 
 # sorted set of words
+# sirali tum kelimeler
+print "-" * 100
+print "Sorted Set of Words (Sirali Kelimeler):", sorted(set(text2))
+
+print "-" * 100
+# Kitapta len(text2) token olmasina ragmen, sadece len(set(text2)) kadar ayri kelime var.
+print "Although it has", len(text2), "tokens, this book has only", len(set(text2)), "distinct words, or word types. "
+
+print "-" * 100
+print "Specific Word Occurrence (word=disappoint):", text2.count('disappoint')
+print "Specific Word Occurrence (word=love):", text2.count('love')
+print "Specific Word Occurrence (word=genius):", text2.count('genius')
+print "Specific Word Occurrence (word=unpleasant):", text2.count('unpleasant')
+
+#  what percentage of the text is taken up by a specific word
+# bir kelimenin tum text icerisinde gecme sikligi
+print "-" * 100
+print "Percentage of Specific Word (word=love):", 100 * float(text2.count('love')) / float(len(text2))
+print "Percentage of Specific Word (word=barbarous):", 100 * float(text2.count('barbarous')) / float(len(text2))
---
+++ ch 01 : Language Processing and Python/03_sentences.py
@@ -0,0 +1,17 @@
+
+""" @SerayBeser
+
+Natural Language Processing with Python
+# http://www.nltk.org/book/ch01.html
+
+Python ile Dogal Dil Isleme
+
+"""
+# Birkac cumle olusturalim.
+# let's create some sentences.
+
+sentence_1 = ['I', 'can', 'not', 'believe', 'how', 'hot', 'it', 'is', 'today', '.']
+sentence_2 = ['I', 'like', 'going ', 'out ', 'to', 'parties ', 'with ', 'friends', '.']
+
+# cumlelerin uzunlugu, kelime sayisi
+print "Length of Sentence 1 :", len(sentence_1)
