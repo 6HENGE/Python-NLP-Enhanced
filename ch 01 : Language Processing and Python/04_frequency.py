921a0ddcc83910a11f5ffa7a4c64dd9445dfe438
6HENGE <160530806+6HENGE@users.noreply.github.com>
2021-10-21 21:40:57 +0000

    Added Unit Tests

--- ch 01 : Language Processing and Python/04_frequency.py
+++ ch 01 : Language Processing and Python/04_frequency.py
@@ -12,4 +12,4 @@ Python ile Dogal Dil Isleme
 # we use text2.
 # text2'yi kullanacagiz.
 # a frequency distribution could be used to record the frequency of each word type in a document.
-# frekans dagilimi, bir belgedeki her bir kelime turunun 
+# frekans dagilimi, bir belgedeki her bir kelime turunun sikligini kaydetmek icin kull
