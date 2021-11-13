dacff28626803582e1f36d346a079729d4cd523f
6HENGE <160530806+6HENGE@users.noreply.github.com>
2021-11-13 17:34:03 +0000

    Update 01-intro.md

--- ch 01 : Language Processing and Python/04_frequency.py
+++ ch 01 : Language Processing and Python/04_frequency.py
@@ -27,4 +27,5 @@ print "Most Common 30 tokens:", vocabulary
 print "-" * 100
 print "Frequency Distribution word='love' : ", frequency_distribution['love']
 print "Frequency Distribution word='hate' : ", frequency_distribution['hate']
-print "Frequency Distribution word='sense' : ", frequency_dis
+print "Frequency Distribution word='sense' : ", frequency_distribution['sense']
+print "Frequency Di
