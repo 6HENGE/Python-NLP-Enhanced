947e0707a137207b31f831d951e218795f62653c
6HENGE <160530806+6HENGE@users.noreply.github.com>
2021-11-27 13:57:03 +0000

    Fixed some compiler warnings.

--- ch 01 : Language Processing and Python/04_frequency.py
+++ ch 01 : Language Processing and Python/04_frequency.py
@@ -30,4 +30,4 @@ print "Frequency Distribution word='hate' : ", frequency_distribution['hate']
 print "Frequency Distribution word='sense' : ", frequency_distribution['sense']
 print "Frequency Distribution word='apathy' : ", frequency_distribution['apathy']
 
-# Plot samples from the freq
+# Plot samples from the frequency distribution displaying the
