52e841789855b23a733c2b6549dcb0feb06c3469
6HENGE <160530806+6HENGE@users.noreply.github.com>
2022-01-02 16:46:16 +0000

    issues fixed

--- ch 01 : Language Processing and Python/05_back_to_python.py
+++ ch 01 : Language Processing and Python/05_back_to_python.py
@@ -12,4 +12,5 @@ Python ile Dogal Dil Isleme
 sentence_1 = ['I', 'can', 'not', 'believe', 'how', 'hot', 'it', 'is', 'today', '.']
 sentence_2 = ['I', 'like', 'going ', 'out ', 'to', 'parties ', 'with ', 'friends', '.']
 
-print "len(word) < 5  :   ", [w for w 
+print "len(word) < 5  :   ", [w for w in sentence_1 if len(w) < 5]
+print "le
