c9d690c5418b7efb9dd949cf783265b3da34c12e
6HENGE <160530806+6HENGE@users.noreply.github.com>
2022-01-08 12:23:01 +0000

    Another version bump

--- ch 01 : Language Processing and Python/05_back_to_python.py
+++ ch 01 : Language Processing and Python/05_back_to_python.py
@@ -13,4 +13,5 @@ sentence_1 = ['I', 'can', 'not', 'believe', 'how', 'hot', 'it', 'is', 'today', '
 sentence_2 = ['I', 'like', 'going ', 'out ', 'to', 'parties ', 'with ', 'friends', '.']
 
 print "len(word) < 5  :   ", [w for w in sentence_1 if len(w) < 5]
-print "len(word) <= 5 :   ", [w for w in sentence_
+print "len(word) <= 5 :   ", [w for w in sentence_1 if len(w) <= 5]
+print "len(word) == 5 
