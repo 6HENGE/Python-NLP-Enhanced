ccfdc0d499d9e7c3dd6c48fa479d05d9753e1b36
6HENGE <160530806+6HENGE@users.noreply.github.com>
2022-03-20 07:35:26 +0000

    minor minor

--- ch 02 : Accessing Text Corpora and Lexical Resources/01_lexical_resources.py
+++ ch 02 : Accessing Text Corpora and Lexical Resources/01_lexical_resources.py
@@ -9,4 +9,6 @@ Python ile Dogal Dil Isleme
 
 """
 A lexicon, or lexical resource, is a collection of words and/or phrases along with associated
-information, such as part-of-speech and sense definitio
+information, such as part-of-speech and sense definitions.
+
+Sozcuksel veya sozlu kaynak, sozcuk v
