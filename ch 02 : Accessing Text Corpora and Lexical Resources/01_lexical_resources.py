25e8b852297b21eefa57bbca533a9a5e07224159
6HENGE <160530806+6HENGE@users.noreply.github.com>
2022-03-20 22:37:56 +0000

    fix typos in comments

--- ch 02 : Accessing Text Corpora and Lexical Resources/01_lexical_resources.py
+++ ch 02 : Accessing Text Corpora and Lexical Resources/01_lexical_resources.py
@@ -13,4 +13,9 @@ information, such as part-of-speech and sense definitions.
 
 Sozcuksel veya sozlu kaynak, sozcuk veya cumlelerin yani sira konusma ve anlam
 tanimlarinin bir parcasi gibi iliskili bilgilerden olusan bir derlemedir. 
-""
+"""
+
+import nltk
+
+
+def unusual_words(text):
