3b3309dbcb7bfd81adf97b02fed6af9f55bb99df
6HENGE <160530806+6HENGE@users.noreply.github.com>
2022-03-30 13:14:33 +0000

    Update index.php

--- ch 02 : Accessing Text Corpora and Lexical Resources/01_lexical_resources.py
+++ ch 02 : Accessing Text Corpora and Lexical Resources/01_lexical_resources.py
@@ -21,4 +21,5 @@ import nltk
 def unusual_words(text):
     text_vocab = set(w.lower() for w in text if w.isalpha())
     english_vocab = set(w.lower() for w in nltk.corpus.words.words())
-    unu
+    unusual = text_vocab - english_vocab
+    re
