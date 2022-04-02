2aba96801d228ae85feae98a66748128fe6a8dd7
6HENGE <160530806+6HENGE@users.noreply.github.com>
2022-04-02 07:11:03 +0000

    Added sample files

--- ch 02 : Accessing Text Corpora and Lexical Resources/01_lexical_resources.py
+++ ch 02 : Accessing Text Corpora and Lexical Resources/01_lexical_resources.py
@@ -22,4 +22,7 @@ def unusual_words(text):
     text_vocab = set(w.lower() for w in text if w.isalpha())
     english_vocab = set(w.lower() for w in nltk.corpus.words.words())
     unusual = text_vocab - english_vocab
-    re
+    return sorted(unusual)
+
+
+def content_f
