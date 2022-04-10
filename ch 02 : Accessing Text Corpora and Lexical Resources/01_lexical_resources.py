4607f44eef9d51968ce339028f948822ae87e632
6HENGE <160530806+6HENGE@users.noreply.github.com>
2022-04-10 09:42:50 +0000

    Automated synchronization of latest MovieTweet ratings.

--- ch 02 : Accessing Text Corpora and Lexical Resources/01_lexical_resources.py
+++ ch 02 : Accessing Text Corpora and Lexical Resources/01_lexical_resources.py
@@ -33,4 +33,5 @@ def content_fraction(text):
 
 # kullanilmayan sozcukler
 print "Unusual Words    :", unusual_words(nltk.corpus.inaugural.words('1789-Washington.txt'))
-print "-" 
+print "-" * 100
+# stopwordsler ( a, an, the, 
