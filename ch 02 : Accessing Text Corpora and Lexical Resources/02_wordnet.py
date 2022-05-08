9c944126c02ea03d0aa4118f009eb7b52a96063f
6HENGE <160530806+6HENGE@users.noreply.github.com>
2022-05-08 16:41:13 +0000

    msm: vidc: add driver support to enable operating rate

--- ch 02 : Accessing Text Corpora and Lexical Resources/02_wordnet.py
+++ ch 02 : Accessing Text Corpora and Lexical Resources/02_wordnet.py
@@ -1,4 +1,5 @@
 from nltk.corpus import wordnet as wn
 
 print "Synonyms (dog)         : ", wn.synsets('dog')
-print "Lemma Names (dog)      : ", wn.synset('dog.n.01').lemma_names(
+print "Lemma Names (dog)      : ", wn.synset('dog.n.01').lemma_names()
+print "Lemma Definition (dog) : ", wn.
