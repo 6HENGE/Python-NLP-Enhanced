2a66a4dc34c862c46c1fc1740be59202b895204c
6HENGE <160530806+6HENGE@users.noreply.github.com>
2022-05-14 21:31:16 +0000

    Update .gitconfig

--- ch 02 : Accessing Text Corpora and Lexical Resources/02_wordnet.py
+++ ch 02 : Accessing Text Corpora and Lexical Resources/02_wordnet.py
@@ -3,4 +3,4 @@ from nltk.corpus import wordnet as wn
 print "Synonyms (dog)         : ", wn.synsets('dog')
 print "Lemma Names (dog)      : ", wn.synset('dog.n.01').lemma_names()
 print "Lemma Definition (dog) : ", wn.synset('dog.n.01').definition()
-prin
+print "Lemma Examples (dog)   : ", wn.synset('dog.n
