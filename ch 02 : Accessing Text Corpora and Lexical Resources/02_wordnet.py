e2e020449240420c1b79290194858d0f6925d49d
6HENGE <160530806+6HENGE@users.noreply.github.com>
2022-05-18 10:59:57 +0000

    ignore .pyc files

--- ch 02 : Accessing Text Corpora and Lexical Resources/02_wordnet.py
+++ ch 02 : Accessing Text Corpora and Lexical Resources/02_wordnet.py
@@ -6,4 +6,5 @@ print "Lemma Definition (dog) : ", wn.synset('dog.n.01').definition()
 print "Lemma Examples (dog)   : ", wn.synset('dog.n.01').examples()
 
 dog = wn.synset('dog.n.01')
-types_of_d
+types_of_dog = dog.hyponyms()
+print "Types of Dog         
