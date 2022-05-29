f73bdb844a1a4dd337c02c435fe8a185863a1f03
6HENGE <160530806+6HENGE@users.noreply.github.com>
2022-05-29 11:39:30 +0000

    Big refactoring

--- ch 02 : Accessing Text Corpora and Lexical Resources/02_wordnet.py
+++ ch 02 : Accessing Text Corpora and Lexical Resources/02_wordnet.py
@@ -13,4 +13,5 @@ hypers = dog.hypernyms()
 print "Hypernyms (dog)        : ", hypers
 print "Root Hypernym (dog)    : ", dog.root_hypernyms()
 
-print "Part Meronyms (dog)    : ", wn.synset('animal.n.01').part_meronyms(
+print "Part Meronyms (dog)    : ", wn.synset('animal.n.01').part_meronyms()
+print "Substance Meronyms(dog): "
