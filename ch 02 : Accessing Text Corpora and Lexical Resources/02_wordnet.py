e706d3b8125e10147ef4353417650236a657c43b
6HENGE <160530806+6HENGE@users.noreply.github.com>
2022-05-29 21:38:03 +0000

    changed gitignore

--- ch 02 : Accessing Text Corpora and Lexical Resources/02_wordnet.py
+++ ch 02 : Accessing Text Corpora and Lexical Resources/02_wordnet.py
@@ -14,4 +14,5 @@ print "Hypernyms (dog)        : ", hypers
 print "Root Hypernym (dog)    : ", dog.root_hypernyms()
 
 print "Part Meronyms (dog)    : ", wn.synset('animal.n.01').part_meronyms()
-print "Substance Meronyms(dog): ", wn.synset('animal.n.01').substance_meronym
+print "Substance Meronyms(dog): ", wn.synset('animal.n.01').substance_meronyms()
+print "Member Holonyms (dog
