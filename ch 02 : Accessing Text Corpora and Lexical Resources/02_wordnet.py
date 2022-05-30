d1fa5cd7836963408f8dff1607d9aca6282b136b
6HENGE <160530806+6HENGE@users.noreply.github.com>
2022-05-30 21:39:11 +0000

    Address PR comments

--- ch 02 : Accessing Text Corpora and Lexical Resources/02_wordnet.py
+++ ch 02 : Accessing Text Corpora and Lexical Resources/02_wordnet.py
@@ -15,4 +15,6 @@ print "Root Hypernym (dog)    : ", dog.root_hypernyms()
 
 print "Part Meronyms (dog)    : ", wn.synset('animal.n.01').part_meronyms()
 print "Substance Meronyms(dog): ", wn.synset('animal.n.01').substance_meronyms()
-print "Member Holonyms (dog)  : ", wn.synset('animal.
+print "Member Holonyms (dog)  : ", wn.synset('animal.n.01').member_holonyms()
+
+print "En
