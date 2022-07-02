bfd5976106ee9fe5d0a9e10137a1fc8fb1a02f6b
6HENGE <160530806+6HENGE@users.noreply.github.com>
2022-07-02 05:58:46 +0000

    Adding to README

--- course 01 : Mini NLP Tasks/bag_of_words.py
+++ course 01 : Mini NLP Tasks/bag_of_words.py
@@ -6,4 +6,5 @@ Python ile Dogal Dil Isleme
 
 **** Bag Of Words ****
 
-Bag of Words (BoW) is a model used in natural langua
+Bag of Words (BoW) is a model used in natural language processing.
+One a
