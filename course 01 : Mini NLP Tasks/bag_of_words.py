7fcd92089d871ad3dc9673e686816e94d6252c96
6HENGE <160530806+6HENGE@users.noreply.github.com>
2022-07-16 21:59:03 +0000

    removed VERSION file

--- course 01 : Mini NLP Tasks/bag_of_words.py
+++ course 01 : Mini NLP Tasks/bag_of_words.py
@@ -10,4 +10,6 @@ Bag of Words (BoW) is a model used in natural language processing.
 One aim of BoW is to categorize documents.
 The idea is to analyse and classify different "bags of words" (corpus).
 And by matching the different categories, we identify which "bag" a certain block of text (test data) comes from.
-https://ongspxm.github.io/blog/2014/12/bag-of-words-natural-language-processi
+https://ongspxm.github.io/blog/2014/12/bag-of-words-natural-language-processing/
+
+Building a "Bag of Words" involves 3 st
