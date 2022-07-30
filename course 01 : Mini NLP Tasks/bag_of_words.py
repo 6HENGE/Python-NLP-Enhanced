1792ced7b8a24271072d2892e70c86b3cc2ed109
6HENGE <160530806+6HENGE@users.noreply.github.com>
2022-07-30 19:26:23 +0000

    1st

--- course 01 : Mini NLP Tasks/bag_of_words.py
+++ course 01 : Mini NLP Tasks/bag_of_words.py
@@ -21,4 +21,5 @@ Kelime Cantasi olusturma 3 asamalidir:
 
 Kelime Cantasi- (Bag of Words) (BoW), dogal dil islemesinde kullanilan bir modeldir.
 BoW'un bir amaci, belgeleri kategorize etmektir. Amac farkli "canta sozcukleri" 'ni (korpus)
-analiz etmek ve siniflandirmaktir. Farkli kategorilere eslestirere
+analiz etmek ve siniflandirmaktir. Farkli kategorilere eslestirerek,
+belirli bir metin blogunun (te
