2cba34b3ffcf24944991ad9374aeef4e6b741304
6HENGE <160530806+6HENGE@users.noreply.github.com>
2022-09-17 18:56:13 +0000

    Fixed directory structure

--- course 01 : Mini NLP Tasks/bag_of_words.py
+++ course 01 : Mini NLP Tasks/bag_of_words.py
@@ -51,4 +51,6 @@ def bag_of_words(texts, mode='count'):
     corpus = cleaning_and_stemming(str(' '.join(texts)).lower(), stemming=False, stopword=False)
     labels = sorted(set(corpus))
     indexes = range(0, len(labels))
-    vocabulary = collections.OrderedDict(sorted(dict(zip(labe
+    vocabulary = collections.OrderedDict(sorted(dict(zip(labels, indexes)).items()))
+
+  
