37e86870d4850a7cf60ebc3e98be0cff8ee08d89
6HENGE <160530806+6HENGE@users.noreply.github.com>
2022-11-05 19:48:27 +0000

    Changes:

--- course 01 : Mini NLP Tasks/bag_of_words.py
+++ course 01 : Mini NLP Tasks/bag_of_words.py
@@ -88,4 +88,4 @@ def demo():
     text_3 = "The quick brown fox jumps over the lazy dog."
     text_4 = "Never jump over the lazy dog quickly."
     print "Manual Bag Of Words :\n", bag_of_words([text_1, text_2, text_3, text_4], mode='count')
-    print "\nScikit-Learn Bag Of Words :\n", bag_of_words_with_scikit_learn([t
+    print "\nScikit-Learn Bag Of Words :\n", bag_of_words_with_scikit_learn([text_1, text_2, text_3, text_4])
