4f8e75235da82d75a9bcf1a5d5669946ce0d5e9d
6HENGE <160530806+6HENGE@users.noreply.github.com>
2022-11-12 18:43:16 +0000

    color

--- course 01 : Mini NLP Tasks/preprocessing.py
+++ course 01 : Mini NLP Tasks/preprocessing.py
@@ -12,3 +12,36 @@ Python ile Dogal Dil Isleme
 from nltk.corpus import gutenberg, stopwords
 from nltk import word_tokenize
 from nltk.stem.porter import PorterStemmer
+
+import string
+
+
+def cleaning(text):
+    """ CLEANING DATA , veri yi temizleyelim.
+    :param text: raw text
+    :return: cleaned wordlist
+    """
+    # 1- Select Only Words, sadece kelimeleri secelim.
+    words = str(text).split()
+
+    # 2- Remove Punctuation, noktalama isaretlerini silelim.
+    for i in range(0, len(words)):
+        for punct in string.punctuation:
+            if str(punct) in words[i]:
+                words[i] = str(words[i]).replace(str(punct), ' ')
+
+    # 3- Normalizing Case, buyuk kucuk harf normalizasyonunu yapalim.
+    words = [w.lower() for w in words]
+
+    # 4- Remove StopWords, stopwords leri kaldiralim.
+    stop_words = ['a', 'about', 'above', 'across', 'after', 'afterwards']
+    stop_words += ['again', 'against', 'all', 'almost', 'alone', 'along']
+    stop_words += ['already', 'also', 'although', 'always', 'am', 'among']
+    stop_words += ['amongst', 'amoungst', 'amount', 'an', 'and', 'another']
+    stop_words += ['any', 'anyhow', 'anyone', 'anything', 'anyway', 'anywhere']
+    stop_words += ['are', 'around', 'as', 'at', 'back', 'be', 'became']
+    stop_words += ['because', 'become', 'becomes', 'becoming', 'been']
+    stop_words += ['before', 'beforehand', 'behind', 'being', 'below']
+    stop_words += ['beside', 'besides', 'between', 'beyond', 'bill', 'both']
+    stop_words += ['bottom', 'but', 'by', 'call', 'can', 'cannot', 'cant']
+    stop_words += ['co', 'computer', 'con', 'could', 'couldnt', 'cry', 'de']
