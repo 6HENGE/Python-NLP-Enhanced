69d3a0161a1605c6ee38d75ad1bcdc1deb1ffd9e
6HENGE <160530806+6HENGE@users.noreply.github.com>
2022-11-22 13:23:06 +0000

    Delete blacktocat.png

--- course 01 : Mini NLP Tasks/text_blob_examples.py
+++ course 01 : Mini NLP Tasks/text_blob_examples.py
@@ -2,4 +2,5 @@ from textblob import TextBlob, Word
 
 text = TextBlob("John likes to watch movies. Mary likes movies too. These are amazing movies. ")
 
-print "Tags            : ",
+print "Tags            : ", text.tags  # nltk -> pos_tag
+print "No
