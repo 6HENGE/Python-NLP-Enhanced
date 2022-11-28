d6b7196711ace2b2f64d2e4c8856fabe94cb852a
6HENGE <160530806+6HENGE@users.noreply.github.com>
2022-11-28 07:44:22 +0000

    Add basic README

--- course 01 : Mini NLP Tasks/text_blob_examples.py
+++ course 01 : Mini NLP Tasks/text_blob_examples.py
@@ -3,4 +3,4 @@ from textblob import TextBlob, Word
 text = TextBlob("John likes to watch movies. Mary likes movies too. These are amazing movies. ")
 
 print "Tags            : ", text.tags  # nltk -> pos_tag
-print "Noun Phrases    : ", text.noun_phrases  #
+print "Noun Phrases    : ", text.noun_phrases  # whic are 'NNP', 'NNI'
