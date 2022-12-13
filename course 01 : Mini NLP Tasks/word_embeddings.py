6c4a4ef7393304db3e6f6e910be8a71d5027a61b
6HENGE <160530806+6HENGE@users.noreply.github.com>
2022-12-13 11:17:43 +0000

    Ignore .idea files

--- course 01 : Mini NLP Tasks/word_embeddings.py
+++ course 01 : Mini NLP Tasks/word_embeddings.py
@@ -4,4 +4,6 @@ Natural Language Processing with Python
 
 Python ile Dogal Dil isleme
 
-**** Word Embeddings (Word2Vec w
+**** Word Embeddings (Word2Vec wit GenSim)  ****
+
+Word embedding is the col
