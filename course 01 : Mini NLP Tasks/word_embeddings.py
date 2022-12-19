5f3526e2335f83af5e12c4c7fd1627e9f5c46430
6HENGE <160530806+6HENGE@users.noreply.github.com>
2022-12-19 08:32:00 +0000

    refine style

--- course 01 : Mini NLP Tasks/word_embeddings.py
+++ course 01 : Mini NLP Tasks/word_embeddings.py
@@ -10,4 +10,4 @@ Word embedding is the collective name for a set of language modeling
 and feature learning techniques in natural language processing (NLP)
 where words or phrases from the vocabulary are mapped to vectors of
 real numbers. Conceptually it involves a mathematical embedding from
-a space with o
+a space with one dimension per word to a continuous 
