4a2453b5fb909b7a6101b7e7bf5361a25bff7d8a
6HENGE <160530806+6HENGE@users.noreply.github.com>
2022-12-22 19:22:02 +0000

    Respond to review comments.

--- course 01 : Mini NLP Tasks/word_embeddings.py
+++ course 01 : Mini NLP Tasks/word_embeddings.py
@@ -13,4 +13,5 @@ real numbers. Conceptually it involves a mathematical embedding from
 a space with one dimension per word to a continuous vector space with
 much lower dimension.
 Methods to generate this mapping include neural networks, dimensionality
-reduction on the word co-occurrence matrix, pr
+reduction on the word co-occurrence matrix, probabilistic models, and
+explicit re
