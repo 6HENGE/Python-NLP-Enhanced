c7cae662df304ba76e4f80f253c6f2b40d9e283d
6HENGE <160530806+6HENGE@users.noreply.github.com>
2023-01-02 12:12:22 +0000

    updating the README

--- course 01 : Mini NLP Tasks/word_embeddings.py
+++ course 01 : Mini NLP Tasks/word_embeddings.py
@@ -23,4 +23,4 @@ parsing and sentiment analysis. https://en.wikipedia.org/wiki/Word2vec
 Word Embedding Algorithms
 1. Embedding Layer
 2. Word2Vec :  Word2vec is a group of related models that are used to produce word embeddings.
-               Word2Vec 
+               Word2Vec kelimeler arasindaki uzakligi vektor
