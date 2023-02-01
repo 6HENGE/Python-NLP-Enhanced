e682bd8f6dd085732e5578048398aefae3f541bc
6HENGE <160530806+6HENGE@users.noreply.github.com>
2023-02-01 21:44:27 +0000

    mm, vmalloc: remove useless variable in vmap_block

--- course 01 : Mini NLP Tasks/word_embeddings.py
+++ course 01 : Mini NLP Tasks/word_embeddings.py
@@ -59,4 +59,5 @@ def demo():
     # Sense and Sensibility by Jane Austen 1811
     text = gutenberg.raw('austen-sense.txt')
     sentences = sent_tokenize(text[:1000])
-    moda
+    modal = word_2_vec_with_gensim(sentences)
+   
