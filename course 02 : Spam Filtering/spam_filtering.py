441a39f255bc22e3cb7450f2100ea0f074fec8d3
6HENGE <160530806+6HENGE@users.noreply.github.com>
2024-04-05 11:08:05 +0000

    msm: mdss: fix issue in calculating decimated dimensions

--- course 02 : Spam Filtering/spam_filtering.py
+++ course 02 : Spam Filtering/spam_filtering.py
@@ -10,3 +10,35 @@ Python ile Dogal Dil Isleme
 
 dataset : http://www2.aueb.gr/users/ion/data/enron-spam/ : Enron 1
 """
+# encoding=utf8
+import sys
+
+import warnings
+import numpy as np
+import collections
+
+from glob import iglob
+from nltk.corpus import stopwords
+from nltk import word_tokenize
+from sklearn.metrics import confusion_matrix
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import LinearSVC
+from keras.preprocessing.text import Tokenizer
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+warnings.filterwarnings('ignore')
+stop_words = set(stopwords.words('english'))
+
+
+def cleaning(text):
+    """ cleaning and normalizing data , veri yi temizleme ve normalize etme.
+    :param text: raw text
+    :return: cleaned and normalized wordlist
+    """
+
+    words = [word for word in word_tokenize(str(text)) if word.isalpha()]
+    return [w.lower() for w in words]
+
+
+class Bag_of_Words:
