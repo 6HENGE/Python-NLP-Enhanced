b9cb22a8ef6b115591863ce4bf1cb3fa4ac38ac5
6HENGE <160530806+6HENGE@users.noreply.github.com>
2024-04-05 18:46:08 +0000

    added help

--- course 02 : Spam Filtering/spam_filtering.py
+++ course 02 : Spam Filtering/spam_filtering.py
@@ -42,3 +42,42 @@ def cleaning(text):
 
 
 class Bag_of_Words:
+    def __init__(self):
+        self.corpus = list()
+        self.labels = list()
+        self.indexes = list()
+        self.vocabulary = collections.OrderedDict()
+
+    def create_vocabulary(self, texts):
+        """ Updates internal vocabulary based on a list of texts.
+
+        :param texts:
+        """
+        print "Creating Vocabulary ...",
+        self.corpus = cleaning(str(' '.join(texts)).lower())
+        self.labels = sorted(set(self.corpus))
+        self.indexes = range(0, len(self.labels))
+        self.vocabulary = collections.OrderedDict(sorted(dict(zip(self.labels, self.indexes)).items()))
+        print "DONE"
+
+    def bag_of_words(self, texts, mode='count'):
+        """ Convert a list of texts to a Numpy matrix.
+
+        :param mode:  one of 'binary', 'count', 'freq', default : 'count'
+        :param texts: list of sentences
+        :return: bag of words (type: numpy ndarray)
+        """
+        print "Creating Bag of Words ...",
+        bows = list()
+        count = 0
+
+        for text in texts:
+            bow = list()
+            text = cleaning(text.lower())
+            for v in self.vocabulary:
+                if str(mode) == 'count':
+                    for t in text:
+                        if str(t) == str(v):
+                            count += 1
+                    bow.append(count)
+                    count = 0
