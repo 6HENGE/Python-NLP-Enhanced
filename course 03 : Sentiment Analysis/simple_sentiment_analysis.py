7729cae2db827623bb824b9322447d5d3ac9d784
6HENGE <160530806+6HENGE@users.noreply.github.com>
2024-04-09 21:04:04 +0000

    Version 0.4.2

--- course 03 : Sentiment Analysis/simple_sentiment_analysis.py
+++ course 03 : Sentiment Analysis/simple_sentiment_analysis.py
@@ -1,2 +1,31 @@
 
 """ @SerayBeser
+
+Natural Language Processing with Python
+
+Python ile Dogal Dil Isleme
+
+**** Simple Sentiment Analysis  ****
+**** Basit Duygu Analizi ****
+"""
+
+from textblob import TextBlob
+from textblob.classifiers import NaiveBayesClassifier
+from afinn import Afinn
+
+
+def sentiment_with_TextBlob(text):
+    sentiments = list()
+    for sentence in text.sentences:
+        sentiments.append(sentence.sentiment)
+    return sentiments
+
+
+def sentiment_with_naive_bayes(train, test, text):
+    cl = NaiveBayesClassifier(train)
+    accuracy_ = cl.accuracy(test)
+    class_ = cl.classify(text)
+    return accuracy_, class_
+
+
+def sentiment_affin(text):
